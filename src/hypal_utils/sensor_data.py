63fa391ea426631fd25da289fd642cbf4148570c
Maksim Shushkevich <m.e.shushkevich@yandex.com>
2026-02-08 19:00:33 +0300

    Update sensor data

--- src/hypal_utils/sensor_data.py
+++ src/hypal_utils/sensor_data.py
@@ -6,7 +6,8 @@ from hypal_utils.candles import Candle_OHLC
 class SensorData(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
-    name: str
+    axis: str
+    sensor: str
     source: str
     candle: Candle_OHLC
     timestamp: int
